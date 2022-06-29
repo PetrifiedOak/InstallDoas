f03891489fce7b615f982f5b52f155daf0ac3bc2
PetrifiedOak <85644556+PetrifiedOak@users.noreply.github.com>
2022-06-29 16:37:53 +0000

    doas.conf was /etc/test.conf im a idiot

--- doas.sh
+++ doas.sh
@@ -5,6 +5,6 @@ $pkgmanager doas
 
 echo "what is your user name?"
 read name
-echo permit nopass $name as root > /etc/test.conf
+echo permit nopass $name as root > /etc/doas.conf
 ln -s /bin/doas /bin/sudo
 
