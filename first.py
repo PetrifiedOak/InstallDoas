d6fbfda6ca2bcc027db20f4137bbe9eebffe0c9b
PetrifiedOak <85644556+PetrifiedOak@users.noreply.github.com>
2022-07-01 12:26:13 +0000

    /etc/test.conf was supposed to be /etc/doas.conf

--- first.py
+++ first.py
@@ -18,12 +18,12 @@ nopass = (input("Do you want nopass? (1 for yes 2 for no)"))
 user = (input("Whats your user named?"))
 
 if nopass == "1":
- file = open("/etc/test1.conf", "w")
+ file = open("/etc/doas.conf", "w")
  file.write("permit nopass " + user + " as root")
  file.close()
 
 if nopass == "2":
- file = open("/etc/test2.conf", "w")
+ file = open("/etc/doas.conf", "w")
  file.write("permit persist " + user + " as root")
  file.close()
 
