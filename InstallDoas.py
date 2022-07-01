0428c55e853dae9746f12e5ff73d078eb1863f54
PetrifiedOak <85644556+PetrifiedOak@users.noreply.github.com>
2022-07-01 13:11:43 +0000

    Changed if to elif and some other stuff

--- InstallDoas.py
+++ InstallDoas.py
@@ -5,15 +5,19 @@ pkg = (input("Whats your package manager? (apt, pacman etc)"))
 if pkg == "emerge":
  subprocess.run(["emerge", "--ask doas"])
 
-if pkg == "apt":
+
+elif pkg == "apt":
  subprocess.run(["apt", "install doas"])
 
-if pkg == "pacman":
+elif pkg == "pacman":
  subprocess.run(["pacman", "-S opendoas"])
- 
- if pkg == "dnf":
-  subprocess.run(["dnf", "install opendoas"])
- 
+
+elif pkg == "dnf":
+    subprocess.run(["dnf", "install opendoas"])
+
+else:
+ print("Package manager not supported!Install it yourself and continue!")
+
 
 
 nopass = (input("Do you want nopass? (1 for yes 2 for no)"))
@@ -21,13 +25,11 @@ nopass = (input("Do you want nopass? (1 for yes 2 for no)"))
 user = (input("Whats your user named?"))
 
 if nopass == "1":
- file = open("/etc/doas.conf", "w")
+ file = open("/etc/test1.conf", "w")
  file.write("permit nopass " + user + " as root")
  file.close()
 
 if nopass == "2":
- file = open("/etc/doas.conf", "w")
+ file = open("/etc/test2.conf", "w")
  file.write("permit persist " + user + " as root")
  file.close()
-
-
